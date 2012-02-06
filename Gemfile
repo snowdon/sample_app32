f665be98f2445b65f524abb4a66f1918097d6625
Xiaochun Shen <snowdonshen@gmail.com>
2012-02-06 15:44:21 +0800

    Finish sign in

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ group :test do
 #  gem 'guard-spork', '0.3.2'
 #  gem 'spork', '~> 0.9.0.rc'
 
+  gem 'cucumber-rails', '1.2.1'
+  gem 'database_cleaner', '0.7.0'
 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 SampleApp32::Application.routes.draw do
   
   resources :users
-
+  resources :sessions, only: [:new, :create, :destory]
 
   get "users/new"
 
   match '/signup',    to: 'users#new'
+  match '/signin',    to: 'sessions#new'
+  match '/signout',   to: 'sessions#destroy'
 
   match '/help',      to: 'static_pages#help'
   match '/about',     to: 'static_pages#about'
